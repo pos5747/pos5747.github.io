1aefb2d3e6264b68115cb95f01d57915b97868b1
Carlisle Rainey <carlislerainey@gmail.com>
2026-08-31 16:34:15 -0400

    clean up repo

--- slides/wk01-slides-material/snippets.mk
+++
@@ -1,2 +0,0 @@
-# wk01 code snippets shown in the deck (checked by `make audit`)
-CODE_SNIPPETS += --code R/lecture-example-01-cg-ols.R load-cg-data
--- slides/wk02-slides-material/snippets.mk
+++
@@ -1,5 +0,0 @@
-# wk02 code snippets shown in the deck (checked by `make audit`)
-CODE_SNIPPETS += --code R/lecture-example-01-beta.R ll-fn-manual
-CODE_SNIPPETS += --code R/lecture-example-01-beta.R ll-fn-dbeta
-CODE_SNIPPETS += --code R/lecture-example-01-beta.R optim-call
-CODE_SNIPPETS += --code R/lecture-example-01-beta.R est-beta-fn
